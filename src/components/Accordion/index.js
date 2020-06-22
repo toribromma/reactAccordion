e708841d5f8accd0ba422e584b4b4a55b8be53b4
toritheterrible <torimpham@gmail.com>
2020-06-22 13:58:02 -0700

    added Chevron to the accordion title

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-
 import "./style.css";
+import Chevron from "../Chevron";
 
 function Accordion(props) {
  return (
    <div className="accordion__section">
      <button className="accordion">
        <p className="accordion__title">{props.title}</p>
+       <Chevron className={"accordion__icon"} width={10} fill={"#777"} />
      </button>
      <div className="accordion__content">
        <div
