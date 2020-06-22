f71641276fea1ace8781bae37237ab471a1e31fa
toritheterrible <torimpham@gmail.com>
2020-06-22 14:14:14 -0700

    added a toggle Accordion function

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Chevron from "../Chevron";
 
 function Accordion(props) {
+    const [setActive, setActiveState] = useState("");
+
+    function toggleAccordion() {
+        setActiveState(setActive === "" ? "active" : "");
+    }
+ 
  return (
    <div className="accordion__section">
-     <button className="accordion">
+     <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
        <p className="accordion__title">{props.title}</p>
        <Chevron className={"accordion__icon"} width={10} fill={"#777"} />
      </button>
