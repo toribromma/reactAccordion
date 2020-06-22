68f29ea631220c7a1f1b60cfcae82c4ce6818e11
toritheterrible <torimpham@gmail.com>
2020-06-22 14:34:58 -0700

    added rotate state that manipulates the css of the chevron icon

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -5,13 +5,14 @@ import Chevron from "../Chevron";
 function Accordion(props) {
     const [setActive, setActiveState] = useState("");
     const [setHeight, setHeightState] = useState("0px");
+    const [setRotate, setRotateState] = useState("accordion__icon")
 
     const content = useRef(null);
 
     function toggleAccordion() {
         setActiveState(setActive === "" ? "active" : "");
-        console.log(content.current.scrollHeight)
-        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`)
+        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
+        setRotateState(setActive === "active" ? "accordion__icon" : "accordion__icon rotate");
     }
 
  
@@ -19,7 +20,7 @@ function Accordion(props) {
    <div className="accordion__section">
      <button className={`accordion ${setActive}`} onClick={toggleAccordion}>
        <p className="accordion__title">{props.title}</p>
-       <Chevron className={"accordion__icon"} width={10} fill={"#777"} />
+       <Chevron className={`${setRotate}`} width={10} fill={"#777"} />
      </button>
      <div ref={content} style={{ maxHeight: `${setHeight}`}} className="accordion__content">
        <div
